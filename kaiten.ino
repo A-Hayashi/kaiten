bc0bd54f2ae0c13faf80d8c8f3e397a8b03f82b2
dracen <a.hayashi0113@gmail.com>
2018-12-11 02:04:30 +0900

    修正

--- kaiten.ino
+++ kaiten.ino
@@ -4,8 +4,48 @@
 
 #include <Wire.h>
 
+//根本
+//黒 GND
+//黄 PWM_IN
+//赤 VCC
+//橙 V_lamp(2) +
+//青 V_lamp(1) -
+
+//ケーブル先端
+//黒 GND
+//黄 PWM_IN
+//赤 VCC
+//青 V_lamp(1) -
+//橙 V_lamp(2) +
+
 byte duty;
 byte lamp_on;
+
+void pwm_init(void)
+{
+  TCCR2A = bit(WGM20) | bit(WGM21) | bit(COM2B1);   //比較一致でLow、BOTTOMでHighをOC2Bﾋﾟﾝへ出力 (非反転動作)
+  TCCR2B = bit(WGM22);  //高速PWM動作, clkT2S/8 (8分周)
+  OCR2A =  99;          //16MHz/(8*(1+99))=20KHz
+  OCR2B =  0;
+  TCCR2B &= ~(bit(CS22) | bit(CS21) | bit(CS20));
+
+  pinMode(KAITEN_PWM, OUTPUT);
+  digitalWrite(KAITEN_PWM, LOW);
+}
+
+void pwm_on(void)
+{
+  TCCR2A = bit(WGM20) | bit(WGM21) | bit(COM2B1);   //比較一致でLow、BOTTOMでHighをOC2Bﾋﾟﾝへ出力 (非反転動作)
+  TCCR2B = bit(WGM22);  //高速PWM動作, clkT2S/8 (8分周)
+  OCR2A =  99;          //16MHz/(8*(1+99))=20KHz
+  OCR2B =  0;
+  TCCR2B |= bit(CS21);
+}
+
+void pwm_off(void)
+{
+  pwm_init();
+}
 
 void setup() {
   pinMode(LAMP, OUTPUT);
@@ -13,13 +53,7 @@ void setup() {
 
   Serial.begin(9600);
   Serial.println("KAITEN Start");
-
-  TCCR2A = bit(WGM20) | bit(WGM21) | bit(COM2B1);   //比較一致でLow、BOTTOMでHighをOC2Bﾋﾟﾝへ出力 (非反転動作)
-  TCCR2B = bit(WGM22);  //高速PWM動作, clkT2S/8 (8分周)
-  OCR2A =  99;          //16MHz/(8*(1+99))=20KHz
-  OCR2B =  0;
-
-  pinMode(KAITEN_PWM, OUTPUT);
+  pwm_init();
 
   duty = 0;
   lamp_on = false;
@@ -27,12 +61,7 @@ void setup() {
   i2c_init();
 }
 
-
-
-
-
 void loop() {
-
   KAITEN_duty(duty);
 
   if (lamp_on == true) {
@@ -40,9 +69,7 @@ void loop() {
   } else {
     LAMP_off();
   }
-
 }
-
 
 void KAITEN_duty(byte duty)
 {
@@ -56,15 +83,14 @@ void KAITEN_duty(byte duty)
 void KAITEN_on(byte duty)
 {
   digitalWrite(KAITEN, LOW);
-  TCCR2B |= bit(CS21);
+  pwm_on();
   OCR2B = duty;
 }
 
 void KAITEN_off()
 {
   digitalWrite(KAITEN, HIGH);
-  TCCR2B &= ~(bit(CS22) | bit(CS21) | bit(CS20));
-  OCR2B = 0;
+  pwm_off();
 }
 
 void LAMP_on()
@@ -77,9 +103,6 @@ void LAMP_off()
   digitalWrite(LAMP, HIGH);
 }
 
-
-
-
 void i2c_init()
 {
   Wire.begin(0x15) ;                 // Ｉ２Ｃの初期化、自アドレスを15とする
@@ -87,8 +110,6 @@ void i2c_init()
   Wire.onReceive(receiveEvent);     // マスタからのデータ送信対応のコールバック関数登録
   Serial.println("i2c slave test");
 }
-
-
 
 // マスターからを受信
 void receiveEvent(int n) {
